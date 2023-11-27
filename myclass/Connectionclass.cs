79f1ad10cb7cd83909813a3ff7af66ea7c13918f
lie699 <145696728+lie699@users.noreply.github.com>
2023-11-28 01:30:07 +0800

    Add files via upload

---
+++ myclass/Connectionclass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace VallenteStudent.myclass
+{
+    internal class Connectionclass
+    {
+        public MySqlConnection connectdb;
+
+        public Connectionclass()
+        {
+            string host = "localhost";
+            string database = "studentsdb";
+            string username = "root";
+            string password = "";
+            string port = "3306";
+
+            string connection_string = "datasource =" + host + ";database=" + database + "; port=" + port + "; username =" + username + ";" +
+                "password=" + password + "; SslMode =none;";
+
+            connectdb = new MySqlConnection(connection_string);
+        }
+    }
+}
